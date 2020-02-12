dba21564a569e2f5e0cb065c37cbffba7d5f2572
Jens Kuhr Jørgensen <jens@journl.dk>
2020-02-12 13:35:38 +0100

    DVL-6 Add idea to gitignore. Restructure php template files and add header and footer files. Update simpleidp-theme to point at kitdocker (like in the VenligLogin project). Add material components web assets to consent module and copy them to the container on docker build. Initial work on styling consentform.php

---
+++ consent-idp/docker/modules/consent/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title><?php echo $headerTitle ?></title>
+
+    <link rel="stylesheet" href="/<?php echo($this->data["baseurlpath"] . 'resources/assets/css/material-fonts.css'); ?>">
+    <link rel="stylesheet" href="/<?php echo($this->data["baseurlpath"] . 'resources/assets/css/material-components-web-v4.0.0.min.css'); ?>">
+    <link rel="stylesheet" href="/<?php echo($this->data["baseurlpath"] . 'resources/assets/css/index.css'); ?>">
+
+    <script src="/<?php echo($this->data["baseurlpath"] . 'resources/assets/js/material-components-web-v4.0.0.min.js'); ?>"></script>
+    <script src="/<?php echo($this->data["baseurlpath"] . 'resources/assets/js/jquery-1.11.3.min.js'); ?>"></script>
+</head>
+<body class="mdc-typography">
